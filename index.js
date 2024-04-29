b96af6d8f321f81e0948ee13a222e98f9cd5d088
Arup Saha <sahaarup911@gmail.com>
2024-04-29 15:40:53 +0600

    added environment and data writing functionalities

--- index.js
+++ index.js
@@ -8,22 +8,25 @@
 
 // dependencies
 const http = require('http');
-
 const { handleReqRes } = require('./helpers/handleReqRes');
+const environment = require('./helpers/environments');
+
+const data = require('./lib/data');
 
 // App object - module scaffolding
 const app = {};
 
-// configuration
-app.config = {
-    port: 3000,
-};
+// testing file system
+data.create('test', 'newFile', { name: 'Bangladesh', Language: 'Bangla' }, (err) => {
+    console.log('error was', err);
+});
 
 // create server
 app.createServer = () => {
     const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, () => {
-        console.log(`listening to port ${app.config.port}`);
+    server.listen(environment.port, () => {
+        console.log(`environment variable is ${process.env.NODE_ENV}`);
+        console.log(`listening to port ${environment.port}`);
     });
 };
 
